1d9ece215a15bf965b91e7af03730af0ff40c63f
Marko Mikec <mikec.marko@protonmail.com>
2021-03-17 16:10:17 +0100

    JS project

--- PHP_MySQL/globe_bank_project/public/index.php
+++ PHP_MySQL/globe_bank_project/public/index.php
@@ -2,11 +2,11 @@
 
 <html lang="en">
   <head>
-    <title></title>
+    <title>Globe Bank project</title>
     <meta charset="utf-8">
   </head>
 
   <body>
-
+   <h1> Globe Bank Comming Soon! </h1>
   </body>
 </html>
