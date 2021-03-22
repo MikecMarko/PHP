10291d62f782a744d78bdbedbc430716180370e0
Marko Mikec <mikec.marko@protonmail.com>
2021-03-22 14:20:47 +0100

    PHP new Project

--- PHP_MySQL/globe_bank_project/private/functions.php
+++ PHP_MySQL/globe_bank_project/private/functions.php
@@ -7,3 +7,23 @@ function url_for($script_path)
     }
     return WWW_ROOT . $script_path;
 }
+
+// Since we will be encoding often in the project we made a shorter functions which will serve the
+// same purpose as urlencode() and rawurlencode().
+
+function u($string = "")
+{
+    return urlencode($string);
+}
+
+function u_raw($string = "")
+{
+    return rawurlencode($string);
+}
+
+// Short function for HTML encoding
+
+function h($string = "")
+{
+    return htmlspecialchars($string);
+}
--- PHP_MySQL/globe_bank_project/public/staff/subjects/show.php
+++ PHP_MySQL/globe_bank_project/public/staff/subjects/show.php
@@ -1,3 +1,15 @@
 <?php
+
+// loading up the functions
+
+require_once '../../../private/initialize.php';
+
+// We can use this thanks to a PHP 7+ COALESCING OPERATOR
 $id = $_GET['id'] ?? '1';
-echo $id;
+echo h($id);
+?>
+
+<br>
+<a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a> <br />
+<a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a> <br />
+<a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a> <br />
