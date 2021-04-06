aa2d3b1f03aa0037f24b79c333aeb22c71865381
Marko Mikec <mikec.marko@protonmail.com>
2021-04-06 15:41:16 +0200

    PHP new project CRUD

--- PHP_MySQL/globe_bank_project/private/validation_functions.php
+++ PHP_MySQL/globe_bank_project/private/validation_functions.php
@@ -12,3 +12,28 @@ function is_blank()
 {
     return !isset($value) || trim($value) === '';
 }
+
+
+/*
+has_preservance('abcd') validates the data presence
+it is reverse of is_blank.
+
+has is a preference for data validation.
+*/
+
+function has_presence($value)
+{
+    return !is_blank($value);
+}
+
+
+/* 
+  has_length_greater_than('abcd', 3) validates the string lenght
+  spaces count towards lenght. we use trim() if spaces should not count
+*/
+
+function has_lenght_greater_than($value, $min)
+{
+    $lenght = strlen($value);
+    return $lenght > $min;
+}
