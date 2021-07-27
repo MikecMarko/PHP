c429e4ea6c3528d689a2919816201f9900c25e01
Marko Mikec <mikec.marko@protonmail.com>
2021-07-27 20:05:19 +0200

    public css

---
+++ PHP_MySQL/globe_bank_project/private/shared/public__footer.php
@@ -0,0 +1,12 @@
+<footer>
+    <p>Copyright <?php echo date('Y') ?>, Globe Bank International</p>
+</footer>
+
+<p>Random text random text>Random text random text >Random text random text >Random text random text >Random text random
+    text >Random text random text >Random text random text</p>
+
+</body>
+
+</html>
+
+<?php db_disconnect($db) ?>
